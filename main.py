b088197d0eeba03f117865ca752eafa8d70ddcb2
Raiyan Islam <raiyan.islam2@student.tdsb.on.ca>
2025-05-31 14:35:34 -0400

    data was added

---
+++ main.py
@@ -0,0 +1,75 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from scipy.signal import find_peaks
+
+def compute_snr(signal, noise):
+    """
+    Computes SNR in decibels from two segments: signal and noise (in linear amplitude).
+
+    Args:
+        signal (np.ndarray): Signal segment (linear scale).
+        noise (np.ndarray): Noise segment (linear scale).
+
+    Returns:
+        float: SNR in dB
+    """
+    signal_power = np.mean(signal**2)
+    noise_power = np.mean(noise**2)
+    snr_db = 10 * np.log10(signal_power / noise_power)
+    return snr_db
+
+
+def get_harmonic_peaks(freqs, magnitudes, num_peaks=5, min_prominence=0.01):
+    peaks, properties = find_peaks(magnitudes, prominence=min_prominence)
+    sorted_indices = np.argsort(magnitudes[peaks])[::-1]
+    top_peaks = peaks[sorted_indices[:num_peaks]]
+    return [(freqs[p], magnitudes[p]) for p in top_peaks]
+
+
+def plot_txt(fig, axs, path, index):
+    df = pd.read_csv(path, delimiter="\t", header=None)
+    
+    filtered_df = df[df[1] != '[-inf]']
+    time = np.array(filtered_df[0], dtype=float)
+    amplitude = np.array(filtered_df[1], dtype=float)
+
+    # axs[index, 0].plot(time, amplitude)
+
+    dt = np.mean(np.diff(time))
+    fs = 1.0 / dt
+
+    linear_amplitude = 10 ** (amplitude / 20.0)
+
+    N = len(linear_amplitude)
+    fft_vals = np.fft.fft(linear_amplitude)
+    freqs = np.fft.fftfreq(N, d=dt)
+
+    fft_offset = 50
+
+    positive_freqs = freqs[fft_offset:N // 2]
+    positive_magnitudes = np.abs(fft_vals[fft_offset:N // 2])
+
+    harmonic_peaks = get_harmonic_peaks(positive_freqs, positive_magnitudes, num_peaks=20, min_prominence=0.5)
+
+    axs[index].plot(positive_freqs, positive_magnitudes)
+    for freq, mag in harmonic_peaks:
+        axs[index].plot(freq, mag, 'ro')
+        axs[index].annotate(f"{freq:.1f} Hz", (freq, mag), textcoords="offset points", xytext=(0, 10), ha='center')
+
+    # print(harmonic_peaks)
+
+    axs[index].set_xscale('log')
+
+
+directory = r"Audacity-Data"
+dir_files = os.listdir(directory)
+
+fig, axs = plt.subplots(len(dir_files), figsize=(12, 20), sharex=True)
+
+for idx, file in enumerate(dir_files):
+    plot_txt(fig, axs, f'{directory}\{file}', index=idx)
+
+plt.tight_layout()
+plt.show()
